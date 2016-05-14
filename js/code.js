d0b2c7b03065946b8d53c95c0ae74f975ffd542e
Mitali <mitalipadwal89@gmail.com>
2016-05-14 17:49:58 +0100

    Displays marker on screen

--- js/code.js
+++ js/code.js
@@ -82,6 +82,9 @@ for (move=0; move<10; move++){
 		}
 	}
 
+document.getElementById(input).innerHTML=player;
+  
+
 	if (board[0][0]==board[0][1]==board[0][2] ||
 		board[1][0]==board[1][1]==board[1][2] ||
 		board[2][0]==board[2][1]==board[2][2] ||
